587edb9af419ee38eb6cb0dde240cb2e3d253a0d
Michael Zhang <michaelzhang@fairfaxmedia.com.au>
2020-09-29 16:39:50 +1000

    Updates

---
+++ precache-manifest.2279dbea5919d8654e0e6a89479461bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4989de802e2ded762d5e76b3ec001271",
+    "url": "/word-space-repetition/index.html"
+  },
+  {
+    "revision": "d738c335727107e224d7",
+    "url": "/word-space-repetition/static/css/main.382eaf45.chunk.css"
+  },
+  {
+    "revision": "507a9125572c27913935",
+    "url": "/word-space-repetition/static/js/2.a7551981.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/word-space-repetition/static/js/2.a7551981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d738c335727107e224d7",
+    "url": "/word-space-repetition/static/js/main.240e210f.chunk.js"
+  },
+  {
+    "revision": "625162aef3be742cb1d2",
+    "url": "/word-space-repetition/static/js/runtime-main.e03d573a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.631af1b03497959e3b3da60df48be285.js"
+  "/word-space-repetition/precache-manifest.2279dbea5919d8654e0e6a89479461bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/word-space-repetition/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
