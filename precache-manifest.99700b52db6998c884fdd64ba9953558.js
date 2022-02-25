e28f925ed2566afd8011d939a5f456ac9713407b
Michael Zhang <michaelzhang@fairfaxmedia.com.au>
2022-02-25 16:18:43 +1100

    Updates

---
+++ precache-manifest.99700b52db6998c884fdd64ba9953558.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3139d6730d4d137d1159e69b9b68b7a6",
+    "url": "/word-space-repetition/index.html"
+  },
+  {
+    "revision": "65cd75b3727e07318d09",
+    "url": "/word-space-repetition/static/css/main.a8ba63c3.chunk.css"
+  },
+  {
+    "revision": "f8a91029d363b28ed490",
+    "url": "/word-space-repetition/static/js/2.d2b2a586.chunk.js"
+  },
+  {
+    "revision": "ff8c2b66e7f6af48a337d6759d3b5618",
+    "url": "/word-space-repetition/static/js/2.d2b2a586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65cd75b3727e07318d09",
+    "url": "/word-space-repetition/static/js/main.e159ca13.chunk.js"
+  },
+  {
+    "revision": "625162aef3be742cb1d2",
+    "url": "/word-space-repetition/static/js/runtime-main.e03d573a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/word-space-repetition/precache-manifest.858352fc024d6d685867afc102b61073.js"
+  "/word-space-repetition/precache-manifest.99700b52db6998c884fdd64ba9953558.js"
 );
 
 self.addEventListener('message', (event) => {
